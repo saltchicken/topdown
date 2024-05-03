d36e82db308df173f4d87f0b8578c2c0b39acbda
saltchicken <john.eicher89@gmail.com>
2024-05-03 13:54:25 -0700

    Create texture class

--- textures/textures.py
+++ textures/textures.py
@@ -1,13 +1,21 @@
 import os
 import pygame
 
+class Texture():
+    def __init__(self, texture_file_path):
+        self.texture_file_path = texture_file_path
+        self.texture = pygame.image.load(texture_file_path)
+    def draw(self, screen, xy):
+        screen.blit(self.texture, (xy[0], xy[1]))
+        
+
 class TextureMaster():
     def __init__(self, profile = ''):
         self.textures = {}
         directory = 'textures/assets/'
         for texture in os.listdir(f'{directory}{profile}'):
             texture_name = os.path.splitext(os.path.basename(texture))[0]
-            self.textures[texture_name] = pygame.image.load(f'{directory}{profile}/{texture}')
+            self.textures[texture_name] = Texture(f'{directory}{profile}/{texture}')
             
     def draw_tile(self, screen, texture_name, x, y):
         try:
@@ -16,7 +24,7 @@ class TextureMaster():
             print("Invalid texture. TODO: Create a default texture. Available textures are:", list(self.textures.keys()))
             # TODO: Probably shouldn't return None. Find better way to handle error like create a default texture
             return None
-        screen.blit(self.textures[texture_name], (x,y))
+        texture.draw(screen, (x,y))
         
     def fill_screen_tile(self, screen, texture_name):
         info = pygame.display.Info()
